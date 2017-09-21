a65c84fc4a7015c6392c0baed25c9c1b438ec6ca
Jay.Chung <wuceh14678@gmail.com>
2017-09-22 00:16:28 +0800

    before beging ejected

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import createStore from './store/createStore'
+import './styles/main.scss'
+
+// Store Initialization
+// ------------------------------------
+const store = createStore(window.__INITIAL_STATE__)
+
+// Render Setup
+// ------------------------------------
+const MOUNT_NODE = document.getElementById('root')
+
+let render = () => {
+  const App = require('./components/App').default
+  const routes = require('./routes/index').default(store)
+
+  ReactDOM.render(
+    <App store={store} routes={routes} />,
+    MOUNT_NODE
+  )
+}
+
+render()
